a6fd94e9e759fab928c995b32276e08a06520871
NatashaK031 <s22210428@student.unklab.ac.id>
2024-11-11 20:14:10 +0700

    Add project files.

---
+++ Tugas 1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d, e, f;
+            Console.Write("Masukkan nilai a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Masukan nilai b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = a + b;
+            d = a - b;
+            e = a * b;
+            f = a / b;
+
+            Console.WriteLine("\nc = hasil pertambahan a + b = {0}", c);
+            Console.WriteLine("\nc = hasil pertambahan a - b = {0}", d);
+            Console.WriteLine("\nc = hasil pertambahan a * b = {0}", e);
+            Console.WriteLine("\nc = hasil pertambahan a / b = {0}", f);
+
+                bool CompareC1, CompareC2, CompareD1, CompareD2, CompareD3, CompareD4;
+            CompareC1 = c < e;
+            CompareC2 = c > e;
+            CompareD1 = d <= f;
+            CompareD2 = d >= f;
+            CompareD3 = c == e;
+            CompareD4 = d != f;
+
+            Console.WriteLine("\nmaka c < e adalah {0}", CompareC1);
+            Console.WriteLine("maka c > e adalah {0}", CompareC2);
+            Console.WriteLine("maka d <= f adalah {0}", CompareD1);
+            Console.WriteLine("maka d >= f adalah {0}", CompareD2);
+            Console.WriteLine("maka c = e adalah {0}", CompareD3);
+            Console.WriteLine("maka d != f adalah {0}", CompareD3);
+
+        }
+    }
+}
